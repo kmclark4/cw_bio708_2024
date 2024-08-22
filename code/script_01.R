bce676d894e0ca1ed2a19883f064fdb258db054e
kmclark4 <kmclark4@uncg.edu>
2024-08-22 10:00:23 -0400

    edit: script 01, test codes
    practice for class 2.

--- code/script_01.R
+++ code/script_01.R
@@ -1,1 +1,13 @@
+#test codes
+
+
 c(1,2)
+
+## produce 100 random numbers that follows a normal distribution
+x <- rnorm(n = 100, mean = 0, sd = 1)
+
+## estimate mean
+mean(x)
+
+## estimate SD
+sd(x)
